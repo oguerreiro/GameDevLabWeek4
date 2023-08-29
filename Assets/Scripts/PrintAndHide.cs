7ad30d5dac63904f825929940aef257c94e806e3
Oliver <100244727+oguerreiro@users.noreply.github.com>
2023-08-29 15:30:14 +1000

    90 percent complete

---
+++ Assets/Scripts/PrintAndHide.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintAndHide : MonoBehaviour
+{
+    public int i;
+    public Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        i = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        i++;
+        Debug.Log($"{gameObject.name}:{i}");
+    }
+}
